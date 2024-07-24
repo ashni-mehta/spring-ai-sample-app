509f23f61e2958e7a57c1f6771136e87c01298ce
ashni <105304831+ashni-mongodb@users.noreply.github.com>
2024-07-24 16:25:32 -0400

    initial commit

---
+++ src/main/java/com/example/spring_ai_sample_app/SpringAiSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_ai_sample_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiSampleAppApplication.class, args);
+	}
+
+}
